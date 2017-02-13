acd5eed0101155295c530672a2c075ffbfe42ee0
Gabriel M <gabrieletudes@gmail.com>
2017-02-13 15:17:16 +0100

    Exercise Two

--- src/js/script.js
+++ src/js/script.js
@@ -6,10 +6,35 @@
  * started at 09/02/2017
  */
 
+let $tabs;
+
+const fHandleTab = function( oEvent ){
+    oEvent.preventDefault();
+    let $elt = oEvent.currentTarget;
+    // check if current link is active
+    if ( $elt.parentNode.classList.contains( "active" ) ) {
+        return;
+    }
+    // remove active class from old active
+    document.querySelector( "ul.nav.nav-tabs .active" ).classList.remove( "active" );
+    // add active class to current
+    $elt.parentNode.classList.add( "active" );
+    // remove active class from old tab content
+    document.querySelector( ".tab-pane.active" ).classList.remove( "active" );
+    // add active class to current's tab target
+    document.getElementById( $elt.getAttribute( "data-tab-target" ) ).classList.add( "active" );
+}
 
 window.addEventListener("load", function(){
     // 1. a with rel=external opens in new window
     Array.from( document.querySelectorAll( 'a[rel*="external"]' ) ).forEach( function( $elt ){
         $elt.setAttribute( "target", "_new" );
     });
+
+    // 2. handle tabs
+    Array.from( document.querySelectorAll( "ul.nav.nav-tabs a" ) ).forEach( function( $elt){
+        $elt.addEventListener( "click", fHandleTab );
+
+    });
+
 });
